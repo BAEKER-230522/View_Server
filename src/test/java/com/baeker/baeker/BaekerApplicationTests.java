cda52a3c5027bf181f93e2032117c0de5b62d46e
choideakook <shdrnrhd113@naver.com>
2023-05-12 17:20:26 +0900

    test: test 용 application.yml 연결 성공

--- src/test/java/com/baeker/baeker/BaekerApplicationTests.java
+++ src/test/java/com/baeker/baeker/BaekerApplicationTests.java
@@ -1,14 +1,22 @@
 package com.baeker.baeker;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BaekerApplicationTests {
 
+	@Value("${custom.con}")
+	private String str;
+
 	@Test
 	void contextLoads() {
+		assertThat(str).isEqualTo("connection success!");
 	}
 
 }
