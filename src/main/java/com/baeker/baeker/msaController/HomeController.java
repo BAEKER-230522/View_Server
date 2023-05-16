17a5065a9132a5922b8d66d5cde6c3a0f65cf30e
choideakook <shdrnrhd113@naver.com>
2023-05-16 09:56:10 +0900

    pull

--- src/main/java/com/baeker/baeker/msaController/HomeController.java
+++ src/main/java/com/baeker/baeker/msaController/HomeController.java
@@ -4,7 +4,6 @@ import com.baeker.baeker.base.request.Rq;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,6 +25,7 @@ public class HomeController {
 
         return "test/login";
     }
+
 
     //-- Config Server Test --//
     @Value("${custom.con}")
